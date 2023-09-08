708e1d3659a236596f017852d8e6b120bb77ca99
savitri <savitri@foxmail.com>
2023-09-08 20:54:24 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -121,4 +121,4 @@ popd
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
